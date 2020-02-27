076248b705025959a3782997d0629274d7dbb962
lyds12 <60756880+lyds12@users.noreply.github.com>
2020-02-26 23:59:18 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
 			
 			
 			//alien.printBoard(); I used to call Stu's board but we're using Sanjita's now so maybe this can be made into a method since it is called in 3 different places and then I can call that method here instead
-			System.out.print("Enter m for move or f for fire: ");
+			System.out.print("Enter m for move or f for fire and S to check your score ");
 			turn = reader.nextLine(); //prompt for what to do 
 			
 			
@@ -35,6 +35,12 @@ public class Main {
 				alien.takeDamage(column_fire);
 				the_ship.printBoard(board);
 				
+			}
+		   
+			if(turn.contentEquals("s") || turn.contentEquals("S")){
+				int column_score = col_score1(null); // get number from attacked ships
+				alien.takeDamage(column_score);
+
 			}
 			else {
 				System.out.println("Invalid input. Try again. ");
@@ -54,11 +60,54 @@ public class Main {
 				System.out.println("That column does not exist. Please enter a column between 0 and 9.");
 			}
 			else {
-				return col_number;
-	
+				
 			}
+				return col_number; 
 		}
-	}
-		
-		
+	
 }
+	// Score Display
+	
+	public static int col_score1(Scanner score){
+		while(true) {
+			System.out.print("Your Score is");
+			
+			int col_number = Integer.parseInt(score.nextLine());
+			
+			if(col_number < 0 || col_number > 9) {
+				System.out.println("That column does not exist. Please enter a column between 0 and 9.");
+			}
+			else {
+				
+			}
+				return col_number; 
+		}
+	
+}
+
+
+
+	//get the score from the column_fire every time f is pressed it regesters the score
+	
+	public static int col_score(Scanner score){
+		while(true) {
+			
+			int column_fire  = col_score1(score);
+			
+			if(column_fire  < 0 || column_fire  > 9) {
+				System.out.println("That column does not exist. Please enter a column between 0 and 9.");
+			}
+			else {
+				return column_fire;
+				
+			}
+	
+}
+
+	}
+	
+	}
+	
+					
+				
+		

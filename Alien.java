d8b4ef986b96110c066cc23388f99aaaebf14fed
smitra33 <60757186+smitra33@users.noreply.github.com>
2020-02-26 20:08:15 -0700

    Newest versions

---
+++ Alien.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Alien {
+	//Initializing the Variables
+	ArrayList<Integer> RowFull = new ArrayList<Integer>();
+	ArrayList<Integer> RowEmpty = new ArrayList<Integer>();
+	ArrayList<ArrayList<Integer>> Board = new ArrayList<ArrayList<Integer>>();
+	
+	//Creating the board
+	public void createBoard(){
+		//creating the row
+		for (int i = 0; i < 10; i++) {
+			RowFull.add(1);
+			RowEmpty.add(0);
+		}
+		//adding the row to the board
+		for (int x = 0; x <4;x++) {
+			Board.add(new ArrayList<Integer>(RowFull));
+		}
+		for (int x = 0; x <6;x++) {
+			Board.add(new ArrayList<Integer>(RowEmpty));
+		}
+	}
+	
+	public void printBoard() {
+		for(ArrayList<Integer> x: this.Board) {
+				System.out.println(x);
+			}
+		}
+	
+
+
+	public int takeDamage(int xPos) {
+		//scans through the 2d array until it has found a 1 then replaces it with a 0
+		for(int i = this.Board.size() - 1; i >= 0; i--){
+			ArrayList<Integer> x = this.Board.get(i);
+			if(x.get(xPos) == 1) {
+				x.add(xPos, 0);
+				x.remove(xPos+1);
+				return 10;
+			}
+		}
+		return 0;
+	}
+}
